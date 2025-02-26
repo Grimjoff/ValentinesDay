0d9c362d868ba6150749a9ebfdada7655302d2f7
BuildTools <unconfigured@null.spigotmc.org>
2025-02-26 01:43:32 +0100

    Adding Placeholder Messages for privacy

---
+++ app/components/letterMessagesEmpty.tsx
@@ -0,0 +1,10 @@
+//This had private messages that I dont want on git, so change those to your liking and rename the file to letterMessages.tsx
+const letterMessages: { [key: string]: string } = {
+    J: "Example",
+    o: "Test",
+    i: "123",
+    n: "Hello",
+    a: "Pew pew"
+};
+
+export default letterMessages;
